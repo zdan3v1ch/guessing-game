31f04ecec676c3b500128d2e437dc2f69fd56783
Anatoli_Z <anatoli.zdanevitch@gmail.com>
2022-08-15 21:56:31 +0300

    add solutiion

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,21 @@
 class GuessingGame {
-    constructor() {}
+    constructor() { }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        return Math.round((this.min + this.max) / 2)
     }
 
     lower() {
-
+        this.max = Math.round((this.min + this.max) / 2)
     }
 
     greater() {
-
+        this.min = Math.round((this.min + this.max) / 2)
     }
 }
 
